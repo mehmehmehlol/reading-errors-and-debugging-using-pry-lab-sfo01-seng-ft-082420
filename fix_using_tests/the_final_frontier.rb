45fb96ea5d390769c63dd746536f23d1bf666a10
Megan Lo <hkmeganlo@gmail.com>
2020-08-02 22:05:15 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -8,12 +8,10 @@ end
 
 
 def state_log(star_date)
-  binding.pry
   "Captain's Log, star date #{star_date}."
 end
 
 def engage
   date = generate_star_date
-  binding.pry
   puts state_log(date)
 end
